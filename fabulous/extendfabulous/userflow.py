925c920a150b3b060f995eab5e6478c9cb3c01d2
Marc-André Tétrault <marc-andre.tetrault@usherbrooke.ca>
2026-04-07 14:55:59 +0200

    Reduced import footprint

--- fabulous/extendfabulous/userflow.py
+++ fabulous/extendfabulous/userflow.py
@@ -1,20 +1,11 @@
-from decimal import Decimal
-from pathlib import Path
-from typing import Any, Optional
 
-import yaml
-from librelane.config.variable import Variable
-from librelane.flows.classic import Classic
-from librelane.flows.flow import Flow, FlowException
+
+
+from librelane.flows.flow import Flow
 from librelane.flows.sequential import SequentialFlow
-from librelane.logging.logger import err, warn
-from librelane.state.state import State
 from librelane.steps import odb as Odb
 from librelane.steps import openroad as OpenROAD
-from librelane.steps.step import Step
 
-from fabulous.fabric_definition.supertile import SuperTile
-from fabulous.fabric_definition.tile import Tile
 from fabulous.fabric_generator.gds_generator.flows.flow_define import (
     check_steps,
     classic_gating_config_vars,
@@ -22,24 +13,10 @@ from fabulous.fabric_generator.gds_generator.flows.flow_define import (
     prep_steps,
     write_out_steps,
 )
-from fabulous.fabric_generator.gds_generator.helper import (
-    get_offset,
-    get_pitch,
-    get_routing_obstructions,
-    round_die_area,
-)
-from fabulous.fabric_generator.gds_generator.steps.add_buffer import AddBuffers
-from fabulous.fabric_generator.gds_generator.steps.custom_pdn import CustomGeneratePDN
-from fabulous.fabric_generator.gds_generator.steps.tile_IO_placement import (
-    FABulousTileIOPlacement,
-)
+
 from fabulous.fabric_generator.gds_generator.steps.tile_optimisation import (
-    OptMode,
     TileOptimisation,
 )
-from fabulous.fabulous_settings import get_context
-
-
 from fabulous.fabric_generator.gds_generator.flows.tile_macro_flow import (
     FABulousTileVerilogMacroFlow,
 )
